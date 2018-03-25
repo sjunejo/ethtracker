c63fe87b285858e6d11252c0f7fd1b6896457785
sjunejo <sjunejo1@gmail.com>
2018-03-25 22:21:52 +0100

    Add method for obtaining Eth price in GBP, and refactor

--- web/ethdata.py
+++ web/ethdata.py
@@ -2,21 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-eth_usd_url = 'https://www.coingecko.com/en/price_charts/ethereum/usd'
+
+__ETH_BASE_URL = 'https://www.coingecko.com/en/price_charts/ethereum/'
+
+_eth_line_regex = re.compile('^.*Ethereum price for today is.*$', re.DOTALL)
+_eth_price_regex = re.compile('[0-9]+\.[0-9]+')
 
 
+# Returns latest price of ETH in USD
 def get_eth_usd():
-    response = requests.get(eth_usd_url)
+    return _get_eth_price('usd')
+
+# Returns latest price of ETH in GBP
+def get_eth_gbp():
+    return _get_eth_price('gbp')
+
+
+def _get_eth_price(currency):
+    # Get HTML page specified by ETH_BASE_URL
+    response = requests.get(__ETH_BASE_URL + currency)
+    # Parse the HTML page to obtain the ETH price data
     soup = BeautifulSoup(response.content, 'html.parser')
     paragraphs = soup.find_all('p')
-
-    eth_line_regex = re.compile('^.*Ethereum price for today is.*$', re.DOTALL)
-    eth_value_regex = re.compile('[0-9]+\.[0-9]+')
     for paragraph in paragraphs:
         paragraph_lines = paragraph.text.split('\n')
         for paragraph_line in paragraph_lines:
-            if eth_line_regex.match(paragraph_line):
-                print(eth_value_regex.search(paragraph_line).group(0))
+            if _eth_line_regex.match(paragraph_line):
+                return _eth_price_regex.search(paragraph_line).group(0)
 
 
-get_eth_usd()
+print(get_eth_usd())
+print(get_eth_gbp())
